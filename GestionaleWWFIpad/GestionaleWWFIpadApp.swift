59bc45882e377cad9b5ff2dfa002abaab1eaf82f
Luca Pagano <149117354+LucaPagan@users.noreply.github.com>
2026-05-09 22:11:42 +0200

    Initial Commit

---
+++ GestionaleWWFIpad/GestionaleWWFIpadApp.swift
@@ -0,0 +1,17 @@
+//
+//  GestionaleWWFIpadApp.swift
+//  GestionaleWWFIpad
+//
+//  Created by Luca Pagano on 09/05/26.
+//
+
+import SwiftUI
+
+@main
+struct GestionaleWWFIpadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
